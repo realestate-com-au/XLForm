0a00956acca5d421aa71156432848cf1d8e031de
Vinnie Liu <vinnie.liu@rea-group.com>
2022-11-24 15:08:35 +1100

    added height property to XLFormRowDescriptor

--- XLForm/XL/Controllers/XLFormViewController.m
+++ XLForm/XL/Controllers/XLFormViewController.m
@@ -718,18 +718,21 @@
 {
     XLFormRowDescriptor *rowDescriptor = [self.form formRowAtIndex:indexPath];
     Class cellClass = [[rowDescriptor cellForFormController:self] class];
-    if ([cellClass respondsToSelector:@selector(formDescriptorCellHeightForRowDescriptor:)]){
-        return [cellClass formDescriptorCellHeightForRowDescriptor:rowDescriptor];
-    }
+	[rowDescriptor cellForFormController:self];
+	CGFloat height = rowDescriptor.height;
+	 if (height != XLFormUnspecifiedCellHeight){
+		 return height;
+	  }
     return self.tableView.rowHeight;
 }
 
 -(CGFloat)tableView:(UITableView *)tableView estimatedHeightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     XLFormRowDescriptor *rowDescriptor = [self.form formRowAtIndex:indexPath];
-    Class cellClass = [[rowDescriptor cellForFormController:self] class];
-    if ([cellClass respondsToSelector:@selector(formDescriptorCellHeightForRowDescriptor:)]){
-        return [cellClass formDescriptorCellHeightForRowDescriptor:rowDescriptor];
+	[rowDescriptor cellForFormController:self];
+	 CGFloat height = rowDescriptor.height;
+	if (height != XLFormUnspecifiedCellHeight){
+	   return height;
     }
     if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"8.0")){
         return self.tableView.estimatedRowHeight;
